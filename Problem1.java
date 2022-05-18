0ef33c580eaeb7f3b9cc45bfa158dbeb2165dcd6
Hitendra Verma <hiten200verma@gmail.com>
2022-05-18 23:40:43 +0530

    change in problem1

--- Problem1.java
+++ Problem1.java
@@ -4,6 +4,7 @@ public class Problem1 {
     int i=2;
     static int j=3;
 	public static void main(String[] args) {
+		
 		//To print non-static  variable inside a main method, we have  to create an object  using new keyword.
 		Problem1 p1=new Problem1();
 		System.out.println(p1.i);
@@ -22,3 +23,9 @@ public class Problem1 {
 	}
 
 }
+// Difference between static and non-static?
+//All the static elements will be loaded into RAM first, and all the non-static elements
+//will be there inside the hard disk in the form of bytecode in the dot class file. now
+//JVM searches for the main
+//method and control start the execution of a java program from the first line of the
+//main method.
